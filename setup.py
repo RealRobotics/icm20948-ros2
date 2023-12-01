a5462e69bb0bd2aaf6d1f8b776ee0e33a9da04c0
Andy Blight <a.j.blight@leeds.ac.uk>
2023-12-01 15:29:29 +0000

    Added resource and launch files

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'icm20948_ros2'
@@ -10,7 +12,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name, ['launch/*.launch.py']),
+        (
+            os.path.join("share", package_name + "/launch"),
+            glob("launch/*launch.[pxy][yma]*"),
+        ),
     ],
     install_requires=[
         'setuptools',
